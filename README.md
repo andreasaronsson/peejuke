53b872896ca172c2afcc8ba776fe02fd9fa9843b
Andreas Aronsson <aron@aron.nu>
2015-05-02 08:56:22 +0200

    Play also.

--- README.md
+++ README.md
@@ -1,4 +1,9 @@
 # peerjuke
 Playing around with java 8 and spark.
 
-curl -X GET http://localhost:4567/ls/%2Fpath%2F
+List:
+  curl -X GET http://localhost:4567/ls/%2Fpath%2F
+
+Play:
+  curl -X GET http://localhost:4567/play/%2Fmnt%2Fmusic%2FA%2FAmorphis%2FElegy%2F01%20Better%20Unborn.mpc
+
--- server/src/main/java/nu/aron/peerjuke/server/Main.java
+++ server/src/main/java/nu/aron/peerjuke/server/Main.java
@@ -9,6 +9,8 @@ import spark.Spark;
 public class Main {
     public static void main(String[] args) {
         Dirlist dirlist = new Dirlist();
+        Player player = new Player();
         Spark.get("/ls/:path", (request, response) -> dirlist.dirList(request.params(":path")));
+        Spark.get("/play/:path", (request, reponse) -> player.play(request.params(":path")));
     }
 }
